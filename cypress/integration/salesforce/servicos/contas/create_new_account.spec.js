1e5424cd07364716ff2f9d08942282319949bdea
Ricardo Sousa <you@example.com>
2020-06-29 07:51:42 -0300

    Adicionando testes

--- cypress/integration/salesforce/servicos/contas/create_new_account.spec.js
+++ cypress/integration/salesforce/servicos/contas/create_new_account.spec.js
@@ -1,43 +1,59 @@
-import { Login } from "./../../pages/login.js"
-import { Home } from "./../../pages/home.js"
+/// <reference types="cypress" />
 
-const login = new Login()
-const home = new Home()
+const nome = 'teste24';
 
-describe('Create a new account', function(){
+describe('APP SALESFORCE', function(){
 
 	beforeEach(() => {
 		//Arrange
-		cy.visit('/')
- 	})
+		cy.request('https://login.salesforce.com/?un=ricardoesousa@hotmail.com&pw=@flavio1979&startURL=%2Fhome%27');
+		cy.visit('https://na111.lightning.force.com/lightning/setup/SetupOneHome/home');
+	  });
 
-	it('Create simple account.', function(){
+	
+	it('Login', function(){
 		//Act
-		login.fillUser('qualidadegft@gft.com')
-		login.fillPass('GFT202005')
-		login.changeAtributteTarget() //Se este atributo não for alterado haverá um redirecionamento de página que fará com que o Cypress se perca nos testes.
-		login.clickLogin()
+		cy.xpath('//span[contains(@class, "uiImage")]').click();
 
 		//Assert
-		home.getConfig().should('have.text', 'Configuração')
-		
-		/*
-    	for (var i = 0; i < 30; i++) {
-    		cy.wait(3000)
-			Cypress.on('window:before:load', (win) => {
-		      Object.defineProperty(win, 'self', {
-		        get: () => {
-		          return window.top
-		        }
-		      })
-		    })	
-		    
-		}
-		*/
+		cy.xpath('//h1/a[@class="profile-link-label"]').invoke('text')
+		.then((text)=>{
+			expect(text).to.equal('Ricardo Sousa'); 
+		});
+	});
 
-		//cy.visit('https://na172.lightning.force.com/lightning/setup/SetupOneHome/home')
+	it('Create an account', function(){
 
+		//Act
+		cy.xpath('//span[text()="Iniciador de aplicativos"]').click({force: true});
+		cy.xpath('//p[text()="Serviço"]').click({force: true});
+		cy.xpath('//span[text()="Contas"]').click({force: true});
+		cy.xpath('//div[@title="Novo(a)"]').click({force: true});
+		cy.xpath('//input[@data-interactive-lib-uid="6"]').type(nome);
+		cy.xpath('//span[text()="Salvar e criar"]').click({force: true});
 
-	})
+		//Assert
+		cy.xpath('//*[contains(@class, "toastMessage")]').invoke('text')
+     	.then((text)=>{
+       	expect(text).to.equal('Conta "'+nome+'" foi criado(a).'); 
+		});
+	});
 
-})
+	it('Delete an account', function(){
+
+		//Act
+		cy.xpath('//span[text()="Iniciador de aplicativos"]').click({force: true});
+		cy.xpath('//p[text()="Serviço"]').click({force: true});
+		cy.xpath('//span[text()="Contas"]').click({force: true});
+		cy.xpath('//a[@title="'+nome+'"]').click({force: true});
+		cy.xpath('//span[text()="Mostrar mais 7 ações"]').click({force: true});
+		cy.xpath('//div[text()="Excluir"]').click({force: true});
+		cy.xpath('//span[text()="Excluir"]').click({force: true});
+
+		//Assert
+		cy.xpath('//*[contains(@class, "toastMessage")]').invoke('text')
+     	.then((text)=>{
+		   expect(text).to.be.oneOf(['Conta "'+nome+'" foi excluído. Desfazer','Account "'+nome+'" foi excluído. Desfazer']); 
+		});
+	});
+});
